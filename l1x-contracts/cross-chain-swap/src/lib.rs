c001b3b7846056e70f4f041c1888aaf97193fa1f
rajesh-l1x <rajesh@l1x.foundation>
2023-08-29 15:05:42 +0530

    [brinup]: node server config updates
    Signed-off-by: rajesh-l1x <rajesh@l1x.foundation>

--- l1x-contracts/cross-chain-swap/src/lib.rs
+++ l1x-contracts/cross-chain-swap/src/lib.rs
@@ -1,96 +1,171 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use l1x_sdk::types::{U128, U64};
+use ethers;
+use ethers::prelude::{parse_log, EthEvent};
+use l1x_sdk::types::{U64};
 use l1x_sdk::{contract, store::LookupMap};
 use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
 
-const STORAGE_CONTRACT_KEY: &[u8] = b"cross-chain-swap-flow";
-const STORAGE_EVENTS_KEY: &[u8] = b"events";
-const STORAGE_STATE_KEY: &[u8] = b"state";
+const STORAGE_CONTRACT_KEY: &[u8; 21] = b"cross-chain-swap-flow";
+const STORAGE_EVENTS_KEY: &[u8; 6] = b"events";
+const STORAGE_STATE_KEY: &[u8; 8] = b"payloads";
 
-const STATE_1: &str = "execute_swap";
-const STATE_2: &str = "finalize_swap";
+const PAYLOAD_1: &str = "execute_swap";
+const PAYLOAD_2: &str = "finalize_swap";
 
-const START_EVENT: &str = "SwapInitiated";
+const INITIATE_EVENT: &str = "SwapInitiated";
 const EXECUTE_EVENT: &str = "SwapExecuted";
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 pub enum Event {
-    SwapInitiated(SwapInitiated),
-    SwapExecuted(SwapExecuted),
+    SwapInitiated(SwapInitiatedEvent),
+    SwapExecuted(ExecuteSwap),
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Clone, Serialize, Deserialize)]
-pub struct SwapInitiated {
-    in_token_address: String,
-    in_amount: U128,
+#[derive(Clone, Debug, EthEvent)]
+#[ethevent(name = "SwapInitiated")]
+struct SwapInitiatedSolidityEvent {
+    #[ethevent(indexed)]
+    global_tx_id: [u8; 32],
+    #[ethevent(indexed)]
+    in_token_address: ethers::types::Address,
+    in_amount: ethers::types::U256,
     source_chain: String,
     destination_chain: String,
-    out_token_address: String,
-    out_amount_min: U128,
-    receiving_address: String,
+    out_token_address: ethers::types::Address,
+    out_amount_min: ethers::types::U256,
+    receiving_address: ethers::types::Address,
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Clone, Serialize, Deserialize)]
-pub struct SwapExecuted {
-    in_token_address: String,
-    user: String,
-    in_amount: U128,
-    out_token_address: String,
-    out_amount: U128,
-    receiving_address: String,
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+pub struct SwapInitiatedEvent {
+    global_tx_id: [u8; 32],
+    in_token_address: l1x_sdk::types::Address,
+    in_amount: l1x_sdk::types::U256,
+    source_chain: String,
+    destination_chain: String,
+    out_token_address: l1x_sdk::types::Address,
+    out_amount_min: l1x_sdk::types::U256,
+    receiving_address: l1x_sdk::types::Address,
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
-pub enum State {
-    ExecuteSwapData(ExecuteSwapData),
-    FinalizeSwapData(FinalizeSwapData),
+#[derive(Clone, Debug, EthEvent,Serialize,Deserialize)]
+#[ethevent(name = "SwapExecuted")]
+pub struct SwapExecutedSolidityEvent {
+    #[ethevent(indexed)]
+    global_tx_id: [u8; 32],
+    user: ethers::types::Address,
+    token_address: ethers::types::Address,
+    amount: ethers::types::U256,
+    receiving_address: ethers::types::Address,
 }
+
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+pub struct ExecuteSwap {
+    global_tx_id: [u8; 32],
+    user: l1x_sdk::types::Address,
+    token_address: l1x_sdk::types::Address,
+    amount: l1x_sdk::types::U256,
+    receiving_address: l1x_sdk::types::Address,
+}
+
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 pub enum Payload {
     ExecuteSwap(ExecuteSwap),
-    FinalizeSwap(FinalizeSwap),
+    FinalizeSwap(FinalizeSwapPayload),
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub enum PayloadResponse {
+    ExecuteSwap(SwapExecutedSolidityEvent),
+    FinalizeSwap(FinalizeSwapSolidityPayload),
+}
+
+#[derive(Clone, Debug, EthEvent,Serialize,Deserialize)]
+#[ethevent(name = "FinalizeSwapPayload")]
+pub struct FinalizeSwapSolidityPayload {
+    #[ethevent(indexed)]
+    global_tx_id: [u8; 32],
+    user: ethers::types::Address,
+}
+
+
+#[derive(BorshSerialize, BorshDeserialize, Clone, Serialize, Deserialize)]
+pub struct FinalizeSwapPayload {
+    global_tx_id: [u8;32],
+    user: l1x_sdk::types::Address,
 }
 
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct CrossChainSwapFlow {
     events: LookupMap<String, Event>,
-    state: LookupMap<String, State>,
+    payloads: LookupMap<String, Payload>,
     total_events: u64,
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Clone, Serialize, Deserialize)]
-pub struct ExecuteSwap {
-    data: ExecuteSwapData,
-    hash: String,
+
+impl From<SwapInitiatedSolidityEvent> for SwapInitiatedEvent {
+    fn from(v: SwapInitiatedSolidityEvent) -> Self {
+        let mut in_amount = vec![0u8; 32];
+        let mut out_amount_min = vec![0u8; 32];
+        v.in_amount.to_little_endian(&mut in_amount);
+        v.out_amount_min.to_little_endian(&mut out_amount_min);
+        Self {
+            global_tx_id: v.global_tx_id,
+            in_token_address: l1x_sdk::types::Address::from(v.in_token_address.0),
+            in_amount: l1x_sdk::types::U256::from_little_endian(&in_amount),
+            source_chain: v.source_chain,
+            destination_chain: v.destination_chain,
+            out_token_address: l1x_sdk::types::Address::from(v.out_token_address.0),
+            out_amount_min: l1x_sdk::types::U256::from_little_endian(&out_amount_min),
+            receiving_address: l1x_sdk::types::Address::from(v.receiving_address.0),
+        }
+    }
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Clone, Serialize, Deserialize)]
-pub struct ExecuteSwapData {
-    global_tx_id: String,
-    token_address: String,
-    amount: U128,
-    receiving_address: String,
+impl From<SwapExecutedSolidityEvent> for ExecuteSwap {
+    fn from(v: SwapExecutedSolidityEvent) -> Self {
+        let mut amount = vec![0u8; 32];
+        v.amount.to_little_endian(&mut amount);
+        Self {
+            global_tx_id: v.global_tx_id,
+            user: l1x_sdk::types::Address::from(v.user.0),
+            token_address: l1x_sdk::types::Address::from(v.token_address.0),
+            amount: l1x_sdk::types::U256::from_little_endian(&amount),
+            receiving_address: l1x_sdk::types::Address::from(v.receiving_address.0),
+        }
+    }
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Clone, Serialize, Deserialize)]
-pub struct FinalizeSwap {
-    data: FinalizeSwapData,
-    hash: String,
+impl From<ExecuteSwap> for SwapExecutedSolidityEvent {
+    fn from(v: ExecuteSwap) -> Self {
+        let mut amount = vec![0u8; 32];
+        v.amount.to_little_endian(&mut amount);
+        Self {
+            global_tx_id: v.global_tx_id,
+            user: ethers::types::Address::from_slice(v.user.as_bytes()),
+            amount: ethers::types::U256::from_little_endian(&amount),
+            token_address: ethers::types::Address::from_slice(v.token_address.as_bytes()),
+            receiving_address: ethers::types::Address::from_slice(v.receiving_address.as_bytes()),
+        }
+    }
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Clone, Serialize, Deserialize)]
-pub struct FinalizeSwapData {
-    global_tx_id: String,
-    user: String,
+impl From<FinalizeSwapPayload> for FinalizeSwapSolidityPayload {
+    fn from(v: FinalizeSwapPayload) -> Self {
+        Self {
+            global_tx_id: v.global_tx_id,
+            user: ethers::types::Address::from_slice(v.user.as_bytes()),
+        }
+    }
 }
 
 impl Default for CrossChainSwapFlow {
     fn default() -> Self {
         Self {
             events: LookupMap::new(STORAGE_EVENTS_KEY.to_vec()),
-            state: LookupMap::new(STORAGE_STATE_KEY.to_vec()),
+            payloads: LookupMap::new(STORAGE_STATE_KEY.to_vec()),
             total_events: u64::default(),
         }
     }
@@ -122,36 +197,62 @@ impl CrossChainSwapFlow {
     pub fn save_event_data(
         global_tx_id: String,
         event_type: String,
-        event_data: Event,
+        source_id: U64,
+        event_data: String,
     ) {
         let mut contract = Self::load();
+        let event_data =
+            base64::decode(event_data.as_bytes()).expect("Can't decode base64 event_data");
 
-        let key = Self::to_key(&global_tx_id, &event_type);
-        contract.events.insert(key, event_data);
-        contract.total_events += 1;
+        match source_id.0 {
+            0 => {
+                let log: ethers::types::Log =
+                    serde_json::from_slice(&event_data).expect("Can't deserialize Log object");
+                let event = parse_log::<SwapInitiatedSolidityEvent>(log)
+                    .expect("Can't parse SwapInitiatedSolidityEvent");
+                let key = Self::to_key(&global_tx_id, &event_type);
+                contract.events.insert(key, Event::SwapInitiated(event.clone().into()));
+                contract.total_events += 1;
+            }
+            1 => {
+                let log: ethers::types::Log =
+                    serde_json::from_slice(&event_data).expect("Can't deserialize Log object");
+                let event: ExecuteSwap = parse_log::<SwapExecutedSolidityEvent>(log)
+                    .expect("Can't parse SwapExecutedSolidityEvent")
+                    .into();
+                let key = Self::to_key(&global_tx_id, &event_type);
+                contract.events.insert(key, Event::SwapExecuted(event.clone().into()));
+                contract.total_events += 1;
+            }
+            _ => panic!("Unknown source id: {}", source_id.0),
+        };
 
-        if contract.state.get(&(global_tx_id.to_owned() + STATE_1)).is_none() {
+        if contract
+            .payloads
+            .get(&(global_tx_id.to_owned() + PAYLOAD_1))
+            .is_none()
+        {
             if let Event::SwapInitiated(start) = contract
                 .events
-                .get(&(global_tx_id.to_owned() + START_EVENT))
+                .get(&(global_tx_id.to_owned() + INITIATE_EVENT))
                 .unwrap()
             {
-                let execute_swap = ExecuteSwapData {
-                    global_tx_id: global_tx_id.clone(),
-                    token_address: start.in_token_address.clone(),
-                    amount: start.in_amount.clone(),
-                    receiving_address: start.receiving_address.clone(),
+                let execute_swap = ExecuteSwap {
+                    global_tx_id: start.global_tx_id,
+                    user: start.receiving_address,
+                    token_address: start.in_token_address,
+                    amount: start.out_amount_min,
+                    receiving_address: start.receiving_address
                 };
-                contract.state.insert(
-                    global_tx_id.to_owned() + STATE_1,
-                    State::ExecuteSwapData(execute_swap),
-                );
+                contract
+                    .payloads
+                    .insert(global_tx_id.to_owned() + PAYLOAD_1, Payload::ExecuteSwap(execute_swap));
             } else {
                 panic!("This is not an SwapInitiated variant.");
             }
         } else if contract
-            .state
-            .get(&(global_tx_id.to_owned() + STATE_2))
+            .payloads
+            .get(&(global_tx_id.to_owned() + PAYLOAD_2))
             .is_none()
         {
             if let Event::SwapExecuted(execute_event) = contract
@@ -159,13 +260,13 @@ impl CrossChainSwapFlow {
                 .get(&(global_tx_id.to_owned() + EXECUTE_EVENT))
                 .unwrap()
             {
-                let finalize_swap = FinalizeSwapData {
-                    global_tx_id: global_tx_id.clone(),
+                let finalize_swap = FinalizeSwapPayload {
+                    global_tx_id: execute_event.global_tx_id,
                     user: execute_event.user.clone(),
                 };
-                contract.state.insert(
-                    global_tx_id.to_owned() + STATE_2,
-                    State::FinalizeSwapData(finalize_swap),
+                contract.payloads.insert(
+                    global_tx_id.to_owned() + PAYLOAD_2,
+                    Payload::FinalizeSwap(finalize_swap),
                 );
             } else {
                 panic!("This is not an SwapExecuted variant.");
@@ -177,49 +278,16 @@ impl CrossChainSwapFlow {
         contract.save();
     }
 
-    pub fn get_payload_to_sign(global_tx_id: String) -> Payload {
+    pub fn get_payload_to_sign(global_tx_id: String) -> PayloadResponse {
         let contract = Self::load();
 
-        if let Some(state) =
-            contract.state.get(&(global_tx_id.to_owned() + STATE_2))
-        {
-            if let State::FinalizeSwapData(data) = state {
-                let mut hasher = Sha256::new();
-                hasher.update(
-                    (data.global_tx_id.clone() + &data.user).as_bytes(),
-                );
-                let hash_result = format!("{:X}", hasher.finalize());
-
-                let output =
-                    FinalizeSwap { data: data.clone(), hash: hash_result };
-
-                return Payload::FinalizeSwap(output);
+        if let Some(payloads) = contract.payloads.get(&(global_tx_id.to_owned() + PAYLOAD_2)) {
+            if let Payload::FinalizeSwap(data) = payloads {
+                return PayloadResponse::FinalizeSwap(data.clone().into());
             }
-        } else if let Some(state) =
-            contract.state.get(&(global_tx_id.to_owned() + STATE_1))
-        {
-            if let State::ExecuteSwapData(data) = state {
-                let mut hasher = Sha256::new();
-
-                // TODO: Need Review - Quick fix to resolve build error
-                // hasher.update(
-                //     (data.global_tx_id.clone()
-                //         + &data.token_address
-                //         + &data.amount.to_string()
-                //         + &data.receiving_address)
-                //         .as_bytes(),
-                // );
-
-                hasher.update(data.global_tx_id.as_bytes());
-                hasher.update(data.token_address.as_bytes());
-                hasher.update(format!("{:#?}", data.amount).as_bytes());
-                hasher.update(data.receiving_address.as_bytes());
-
-                let hash_result = format!("{:X}", hasher.finalize());
-
-                let output =
-                    ExecuteSwap { data: data.clone(), hash: hash_result };
-                return Payload::ExecuteSwap(output);
+        } else if let Some(payloads) = contract.payloads.get(&(global_tx_id.to_owned() + PAYLOAD_1)) {
+            if let Payload::ExecuteSwap(data) = payloads {
+                return PayloadResponse::ExecuteSwap(data.clone().into());
             }
         }
         panic!("invalid global transaction id: {}", global_tx_id);
